16373f56368617a2c2dab82ad2be08817fd6339c
Milfren <nerflimjohn@gmail.com>
2019-10-10 17:41:18 +1000

    setup structure

---
+++ src/components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+	<v-row no-gutters class="grey darken-4 white--text" style="height: 100%">
+		<v-col cols="12" sm="7" class="pa-5">
+			<p class="font-thin title">Coffee</p>
+
+			<!-- menu -->
+			<v-row>
+				<v-col cols="12" sm="3">test</v-col>
+				<v-col cols="12" sm="3">test</v-col>
+				<v-col cols="12" sm="3">test</v-col>
+				<v-col cols="12" sm="3">test</v-col>
+				<v-col cols="12" sm="3">test</v-col>
+				<v-col cols="12" sm="3">test</v-col>
+				<v-col cols="12" sm="3">test</v-col>
+				<v-col cols="12" sm="3">test</v-col>
+				<v-col cols="12" sm="3">test</v-col>
+				<v-col cols="12" sm="3">test</v-col>
+				<v-col cols="12" sm="3">test</v-col>
+				<v-col cols="12" sm="3">test</v-col>
+				<v-col cols="12" sm="3">test</v-col>
+			</v-row>
+		</v-col>
+		<v-col cols="12" sm="5" class="grey darken-3 elevation-5">
+			<div class="pa-5">
+				<p class="font-thin title">Menu</p>
+
+				<!-- menu -->
+				<v-row>
+					<v-col cols="12" sm="4">test</v-col>
+					<v-col cols="12" sm="4">test</v-col>
+					<v-col cols="12" sm="4">test</v-col>
+					<v-col cols="12" sm="4">test</v-col>
+					<v-col cols="12" sm="4">test</v-col>
+					<v-col cols="12" sm="4">test</v-col>
+					<v-col cols="12" sm="4">test</v-col>
+					<v-col cols="12" sm="4">test</v-col>
+					<v-col cols="12" sm="4">test</v-col>
+				</v-row>
+			</div>
+
+			<NavBar />
+		</v-col>
+	</v-row>
+</template>
+
+
+<script>
+import NavBar from './Nav';
+
+export default {
+	components: { NavBar }
+};
+</script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,42 @@
+<template>
+	<v-navigation-drawer permanent dark mini-variant app right color="grey darken-4">
+		<v-list>
+			<v-list-item-group v-model="model" color="brown lighten-3">
+				<v-list-item v-for="(item, i) in items" :key="i">
+					<v-list-item-icon>
+						<v-icon v-text="item.icon"></v-icon>
+					</v-list-item-icon>
+					<v-list-item-content>
+						<v-list-item-title v-text="item.text" />
+					</v-list-item-content>
+				</v-list-item>
+			</v-list-item-group>
+		</v-list>
+	</v-navigation-drawer>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		items: [
+			{
+				icon: 'mdi-inbox',
+				text: 'Inbox'
+			},
+			{
+				icon: 'mdi-star',
+				text: 'Star'
+			},
+			{
+				icon: 'mdi-send',
+				text: 'Send'
+			},
+			{
+				icon: 'mdi-email-open',
+				text: 'Drafts'
+			}
+		],
+		model: 1
+	})
+};
+</script>
